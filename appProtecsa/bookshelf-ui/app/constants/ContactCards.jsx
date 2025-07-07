723189e4bc4c4cc089164cb2709d20f0c16d314a
RodGonzalez <185538443+UliRodGonzZa@users.noreply.github.com>
2025-07-06 23:48:58 -0600

    Update ContactCards.jsx

--- appProtecsa/bookshelf-ui/app/constants/ContactCards.jsx
+++ appProtecsa/bookshelf-ui/app/constants/ContactCards.jsx
@@ -7,25 +7,25 @@ export const cards = [
   {
     icon: GitHubIcon,
     label: 'GitHub',
-    url: 'https://github.com/tuusuario',
+    url: 'https://github.com/UliRodGonzZa',
     hoverColor: '#333333',
   },
   {
     icon: FacebookIcon,
     label: 'Facebook',
-    url: 'https://www.facebook.com/tuusuario',
+    url: 'https://github.com/UliRodGonzZa',
     hoverColor: '#3B5998',
   },
   {
     icon: InstagramIcon,
     label: 'Instagram',
-    url: 'https://www.instagram.com/tuusuario',
+    url: 'https://github.com/UliRodGonzZa',
     hoverColor: '#E1306C',
   },
   {
     icon: DescriptionIcon,
     label: 'Notion',
-    url: 'https://www.notion.so/tuusuario',
+    url: 'https://www.notion.so/Rodrigo-Study-Planner-1a67fe043af2805b9a02d88794ea9482?source=copy_link',
     hoverColor: '#000000',
   },
 ];
